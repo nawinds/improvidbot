3f82f09688f76b33a4efaaf1df1789c33b53df9a
naksenov <naksenov0513@gmail.com>
2021-08-10 18:57:39 +0300

    aiogram added in functions with codes

--- modules/commands/admin/codes/pr_codes.py
+++ modules/commands/admin/codes/pr_codes.py
@@ -1,5 +1,5 @@
 from config import ADMIN_ID
-from modules.bot import bot
+from modules.bot import bot, dp
 from modules.logger import get_logger
 from modules.states import change_state, get_state
 from modules.commands.admin.codes.new_code_title import new_code_title
@@ -15,8 +15,8 @@ import string
 pr_codes_logger = get_logger("admin_pr_codes_commands")
 
 
-@bot.message_handler(commands=["new_pr_code"], func=lambda message: main_admin(message))
-def new_pr_code(message):
+@dp.message_handler(commands=["new_pr_code"], function=lambda message: main_admin(message))
+async def new_pr_code(message):
     code_db = True
     letters = string.ascii_lowercase
     session = db_session.create_session()
@@ -32,18 +32,18 @@ def new_pr_code(message):
     change_state(message, NEW_PR_CODE_TITLE_STATE)
 
 
-@bot.message_handler(commands=["edit_pr_title"], func=lambda m: main_admin(m))
-def edit_pr_title(message):
-    new_code_title(message, " ".join(message.text.split()[1:]), 'pr')
+@dp.message_handler(commands=["edit_pr_title"], function=lambda m: main_admin(m))
+async def edit_pr_title(message):
+    await new_code_title(message, " ".join(message.text.split()[1:]), 'pr')
 
 
-@bot.message_handler(content_types=["text"], func=lambda m: get_state(m) == NEW_PR_CODE_TITLE_STATE)
-def new_pr_code_title(message):
-    new_code_title(message, message.text, 'pr')
+@dp.message_handler(content_types=["text"], function=lambda m: get_state(m) == NEW_PR_CODE_TITLE_STATE)
+async def new_pr_code_title(message):
+    await new_code_title(message, message.text, 'pr')
 
 
-@bot.message_handler(commands=["all_pr_codes"], func=lambda message: main_admin(message))
-def all_pr_codes(message):
+@dp.message_handler(commands=["all_pr_codes"], function=lambda message: main_admin(message))
+async def all_pr_codes(message):
     session = db_session.create_session()
     codes = session.query(Code).filter(Code.type == 2).all()
     text = "Список pr-кодов:\n"
@@ -60,4 +60,4 @@ def all_pr_codes(message):
     for i in used_data:
         for u in data[i]:
             text += u
-    bot.send_message(ADMIN_ID, text)
+    await bot.send_message(ADMIN_ID, text)
